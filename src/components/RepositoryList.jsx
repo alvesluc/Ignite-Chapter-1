992e44a6613d3faba0f0e16586907267ac121ff0
Lucas Alves <l.alves001@gmail.com>
2021-03-26 20:14:33 -0300

    Created RepositoryItem component, and added props to it

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,16 +1,20 @@
+import { RepositoryItem } from "./RepositoryItem";
+
+const repository = {
+  name: "form",
+  description: "Teste",
+  link: "http://github.com",
+};
+
 export function RepositoryList() {
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <li>
-          <strong>Repositório</strong>
-          <p>Forms in React</p>
-
-          <a href="">
-            Acessar repositório
-          </a>
-        </li>
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
+        <RepositoryItem repository={repository} />
       </ul>
     </section>
   );
