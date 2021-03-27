7fce82e8a3df7b5f92d8bdf243e89a951842eeab
Lucas Alves <l.alves001@gmail.com>
2021-03-26 22:13:45 -0300

    Fechting data from github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,7 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss'
+import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 const repository = {
   name: "form",
@@ -9,6 +10,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/alvesluc/repos")
+      .then((response) => response.json())
+      .then((data) => setRepositories(data));
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
